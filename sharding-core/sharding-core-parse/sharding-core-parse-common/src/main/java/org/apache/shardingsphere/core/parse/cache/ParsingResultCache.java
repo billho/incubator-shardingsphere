619ad49542f8dc674cf3a73a1ef15c7ba53aaa63
bill_ho <bill_ho@foxmail.com>
2019-11-05 17:52:35 +0800

    fix 因高并发引起的hasmap 扩容引起的进程死锁

--- sharding-core/sharding-core-parse/sharding-core-parse-common/src/main/java/org/apache/shardingsphere/core/parse/cache/ParsingResultCache.java
+++ sharding-core/sharding-core-parse/sharding-core-parse-common/src/main/java/org/apache/shardingsphere/core/parse/cache/ParsingResultCache.java
@@ -17,10 +17,13 @@
 
 package org.apache.shardingsphere.core.parse.cache;
 
+import org.apache.commons.collections4.map.AbstractHashedMap;
 import org.apache.commons.collections4.map.AbstractReferenceMap;
 import org.apache.commons.collections4.map.ReferenceMap;
 import org.apache.shardingsphere.core.parse.antlr.sql.statement.SQLStatement;
 
+import java.lang.reflect.Field;
+import java.util.Collections;
 import java.util.Map;
 
 /**
@@ -30,17 +33,45 @@ import java.util.Map;
  * @author zhaojun
  */
 public final class ParsingResultCache {
-    
-    private final Map<String, SQLStatement> cache = new ReferenceMap<>(AbstractReferenceMap.ReferenceStrength.SOFT, AbstractReferenceMap.ReferenceStrength.SOFT, 65535, 1);
-    
+
+    /**
+     * fix 因高并发引起的hasmap 扩容引起的进程死锁
+     */
+    private final Map<String, SQLStatement> cache =
+//            Collections.synchronizedMap(
+                    new ReferenceMap<String, SQLStatement>(AbstractReferenceMap.ReferenceStrength.SOFT, AbstractReferenceMap.ReferenceStrength.SOFT, 65535, 1)
+//            )
+            ;
     /**
      * Put SQL and parsing result into cache.
      * 
      * @param sql SQL
      * @param sqlStatement SQL statement
      */
+    int mythreshold = 65000 ; //大概是 65536 * 0.9
     public void put(final String sql, final SQLStatement sqlStatement) {
-        cache.put(sql, sqlStatement);
+        if (cache.size()>mythreshold) {
+            this.mythreshold = getThreshold();
+            synchronized (cache) {
+                cache.put(sql, sqlStatement);
+            }
+        }
+        else{
+            cache.put(sql, sqlStatement);
+        }
+    }
+
+    private int getThreshold(){
+        try {
+            Class clz = AbstractHashedMap.class;
+            Field f = clz.getDeclaredField("threshold");
+            f.setAccessible(true);
+            return f.getInt(this.cache)-500;
+        }
+        catch ( Exception e){
+            e.printStackTrace();
+            return  mythreshold;
+        }
     }
     
     /**
@@ -59,4 +90,12 @@ public final class ParsingResultCache {
     public synchronized void clear() {
         cache.clear();
     }
+
+    /**
+     * test
+     * @param args
+     */
+//    public static void main(String[] args) {
+//        System.out.println(new ParsingResultCache().getThreshold());
+//    }
 }
